35b74e7a1c8e17a93a9afa7bed1bc781092d1358
Ganne Joshika Chowdary <60805330+joshikag@users.noreply.github.com>
2021-10-05 00:19:30 +0530

    Update Head.jsx

--- components/Head.jsx
+++ components/Head.jsx
@@ -17,8 +17,8 @@ const CustomHead = ({ title }) => {
         property="og:description"
         content="A web developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      
+      <meta property="og:url" content="https://joshika-ganne.vercel.app/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
