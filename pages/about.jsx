39d311e8b570f5f64f4310bec70102dcc400d658
Ganne Joshika Chowdary <60805330+joshikag@users.noreply.github.com>
2021-10-05 11:37:41 +0530

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,4 +1,4 @@
-import styles from '../styles/GithubPage.module.css';
+import styles from '../styles/ContactPage.module.css';
 import GitHubCalendar from 'react-github-calendar';
 
 
@@ -12,9 +12,9 @@ const AboutPage = () => {
     level4: '#39d353',
   };
   return (
-    <>
   
-        <div className={styles.about}>
+  <>
+       
           {/* <Image
             src={user.avatar_url}
             className={styles.avatar}
@@ -22,56 +22,58 @@ const AboutPage = () => {
             width={50}
             height={50}
           /> */}
+          <div >
+            <div>
           <h3 >Hi! This is Joshika Ganne</h3>
-        </div >
-        <div className={styles.about}>
-          <p>
+        
+        
+          <p >
           A self-motivated and active tech enthusiast that enjoys experimenting with new technologies, creating new things, and applying her technological skills to address real-world problems.
           </p>
-        </div>
-        <div className={styles.about}>
-          <h3>
+    
+        
+          <h3 >
          Education:
           </h3>
-          <p>
+          <p >
             Vellore Institute of Technology (Batch of 2023)
           </p>
-          <p> B.Tech- Computer Science and Engineering</p>
-        </div>
-        <div className={styles.about}>
-          <h3>Skills and Interests:</h3>
-          <ul className={styles.about}>
+          <p > B.Tech- Computer Science and Engineering</p>
+     
+      
+          <h3 >Skills and Interests:</h3>
+          <ul >
             <li>Web Development</li>
             <li>React.js</li>
             <li>Data Structures and Algorithms</li>
             <li>UI/UX</li>
             <li>Data Science</li>
           </ul>
-        </div>
-
-        <div className={styles.about}>
-          <a className={styles.underline} target="_blank"href="https://drive.google.com/file/d/1Ah6YaYMrE9NtDLIKnRgpCBqzaU0MNK_3/view?usp=sharing">
+      
+          <a className={styles.underline} target="_blank"href="https://drive.google.com/file/d/1Ah6YaYMrE9NtDLIKnRgpCBqzaU0MNK_3/view?usp=sharing" >
           <h3>Go to Resume</h3>
           </a>
-        </div>
-        
-        
-      
-      
+       
+     <br></br>
      
-      <div className={styles.about}>
-      <a className={styles.underline} target="_blank"href="https://github.com/joshikag">
+      <a className={styles.underline} target="_blank"href="https://github.com/joshikag" >
         <h3>View Github</h3>
         </a>
+        <div >
         <GitHubCalendar
           username='joshikag'
           theme={theme}
           hideColorLegend
         />
-      </div>
-
+        </div>
+        
+       
+          </div>
+          </div>
+        
+      </>
  
-    </>
+    
     
   );
 };
@@ -83,4 +85,5 @@ export async function getStaticProps() {
 }
 
 export default AboutPage;
+
 
